dd37b2c6c2af98d6b9866ba355c3f1e20df71cd2
kaushaln1 <54100570+kaushaln1@users.noreply.github.com>
2021-06-24 21:24:04 +0530

    initial

---
+++ src/main/java/com/app/digiImmu/DigiImmuApplication.java
@@ -0,0 +1,13 @@
+package com.app.digiImmu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiImmuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiImmuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/digiImmu/DigiImmuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.digiImmu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiImmuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
